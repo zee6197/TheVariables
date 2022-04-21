d58e753c6808379e5409005aa446ccad79cfef5a
zee6197 <zeeman@berkeley.edu>
2022-04-21 12:35:00 -0700

    First commit

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace VariablesExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Hi = "SDE-1";
+            int num= 101;
+            char Hey = 'A';
+            bool AreYouSde= true;
+            double TheDec = 10.0;
+            decimal num2 = 10000;
+            long num3 = 1000000000000;
+
+            Console.WriteLine();
+            Console.WriteLine($"Hello, {Hi}. Welcome to Software Engineering {num}{Hey}!");
+            Console.WriteLine($"Are you an SDE? {AreYouSde}. If so what's your power? {num2}. What's your rank? {TheDec}");
+            Console.WriteLine($"Ambition level? {num3}");
+
+        }
+    }
+}
